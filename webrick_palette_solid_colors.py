931d36d06569f22f4b00e8af09b4d818d6ce5d67
DE\KMT2ABT <martin.kastner@de.bosch.com>
2026-08-20 09:01:23 +0000

    Farbpallte solid_colors.py

---
+++ webrick_palette_solid_colors.py
@@ -0,0 +1,28 @@
+"""WeBrick Solid Color palette.
+
+Ausschließlich normale Vollfarben (Solid Color).
+Keine transparenten, metallischen oder Unique/Special-Farben.
+
+Quelle: WeBrick Color Chart / Solid Color filter.
+https://www.webrick.com/color-chart
+"""
+
+WEBRICK_SOLID_PALETTE = {
+    (27,42,52): {"webrick_id":26,"ldraw_id":0,"lego_id":11,"rebrickable_id":26,"bricklink_id":0,"name":"Black","de":"Schwarz"},
+    (242,243,242): {"webrick_id":1,"ldraw_id":15,"lego_id":1,"rebrickable_id":15,"bricklink_id":15,"name":"White","de":"Weiß"},
+    (180,0,29): {"webrick_id":21,"ldraw_id":4,"lego_id":5,"rebrickable_id":4,"bricklink_id":4,"name":"Red","de":"Rot"},
+    (144,31,118): {"webrick_id":124,"ldraw_id":26,"lego_id":71,"rebrickable_id":26,"bricklink_id":26,"name":"Magenta","de":"Magenta"},
+    (245,205,47): {"webrick_id":24,"ldraw_id":14,"lego_id":3,"rebrickable_id":14,"bricklink_id":14,"name":"Yellow","de":"Gelb"},
+    (0,156,68): {"webrick_id":28,"ldraw_id":2,"lego_id":6,"rebrickable_id":2,"bricklink_id":2,"name":"Green","de":"Grün"},
+    (0,76,146): {"webrick_id":23,"ldraw_id":1,"lego_id":7,"rebrickable_id":1,"bricklink_id":1,"name":"Blue","de":"Blau"},
+    (13,105,171): {"webrick_id":102,"ldraw_id":73,"lego_id":42,"rebrickable_id":73,"bricklink_id":73,"name":"Medium Blue","de":"Mittelblau"},
+    (112,121,101): {"webrick_id":151,"ldraw_id":378,"lego_id":48,"rebrickable_id":378,"bricklink_id":378,"name":"Sand Green","de":"Sandgrün"},
+    (132,124,111): {"webrick_id":135,"ldraw_id":379,"lego_id":55,"rebrickable_id":379,"bricklink_id":379,"name":"Sand Blue","de":"Sandblau"},
+    (95,49,9): {"webrick_id":192,"ldraw_id":70,"lego_id":88,"rebrickable_id":70,"bricklink_id":70,"name":"Reddish Brown","de":"Rotbraun"},
+    (221,196,150): {"webrick_id":5,"ldraw_id":19,"lego_id":2,"rebrickable_id":19,"bricklink_id":19,"name":"Tan","de":"Tan / Beige"},
+    (178,117,89): {"webrick_id":138,"ldraw_id":28,"lego_id":69,"rebrickable_id":28,"bricklink_id":28,"name":"Dark Tan","de":"Dunkelbeige"},
+    (246,173,151): {"webrick_id":283,"ldraw_id":78,"lego_id":90,"rebrickable_id":78,"bricklink_id":78,"name":"Light Flesh","de":"Heller Hautton"},
+    (204,112,42): {"webrick_id":312,"ldraw_id":84,"lego_id":150,"rebrickable_id":84,"bricklink_id":84,"name":"Medium Dark Flesh","de":"Medium Dark Flesh / Medium Nougat"},
+}
+
+WEBRICK_SOLID_COLOR_COUNT = len(WEBRICK_SOLID_PALETTE)
